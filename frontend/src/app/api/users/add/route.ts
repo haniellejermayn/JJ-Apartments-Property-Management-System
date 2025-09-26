fa4a5a37428b62de133c29bf3edaf7795375c973
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-09-26 16:01:17 +0800

    fix: used env variable for fetch users

--- frontend/src/app/api/users/add/route.ts
+++ frontend/src/app/api/users/add/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
 
-    const response = await fetch('http://localhost:8080/api/users/add', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/add`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- frontend/src/app/api/users/login/route.ts
+++ frontend/src/app/api/users/login/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
 
-    const response = await fetch('http://localhost:8080/api/users/login', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/users/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
